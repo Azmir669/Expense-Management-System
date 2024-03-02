f25edf1096bec1d6f6d16635e0ccc9bd93f1b9b2
DUDEKULA AZMEER VALI <97158644+Azmir669@users.noreply.github.com>
2024-03-02 18:09:50 +0530

    Delete expense-tracker-firebase-react directory

--- expense-tracker-firebase-react/src/hooks/useGetUserInfo.js
+++
@@ -1,6 +0,0 @@
-export const useGetUserInfo = () => {
-  const { name, profilePhoto, userID, isAuth } =
-    JSON.parse(localStorage.getItem("auth")) || {};
-
-  return { name, profilePhoto, userID, isAuth };
-};
